0d45a91a4865b2603b95351037588308acebf3a7
Kasualix <670067575@qq.com>
2023-09-03 02:18:33 +0800

    use forge event

--- src/main/java/com/teampotato/potattributes/event/AttributeEvents.java
+++ src/main/java/com/teampotato/potattributes/event/AttributeEvents.java
@@ -1,11 +1,15 @@
 package com.teampotato.potattributes.event;
 
 import com.teampotato.potattributes.registry.ModAttributes;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeInstance;
+import net.minecraftforge.event.entity.EntityAttributeModificationEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.registries.RegistryObject;
 
 public class AttributeEvents {
     @SubscribeEvent(priority = EventPriority.LOWEST)
@@ -15,5 +19,12 @@ public class AttributeEvents {
         if (attributeInstance == null || livingEntity.level.isClientSide || event.isCanceled()) return;
         double damageReduction = attributeInstance.getValue();
         event.setAmount(event.getAmount() * (1.0F - (float) damageReduction));
+    }
+
+    @SubscribeEvent
+    public static void registerAttributes(EntityAttributeModificationEvent event) {
+        for (RegistryObject<Attribute> attributeRegistryObject : ModAttributes.ATTRIBUTES.getEntries()) {
+            event.add(EntityType.PLAYER, attributeRegistryObject.get());
+        }
     }
 }
