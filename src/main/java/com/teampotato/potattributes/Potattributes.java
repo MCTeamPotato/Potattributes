685a70db3ad6669ef9a13b0e56195e2dbc0ccd85
Kasualix <670067575@qq.com>
2023-09-25 22:01:50 +0800

    fix registry

--- src/main/java/com/teampotato/potattributes/Potattributes.java
+++ src/main/java/com/teampotato/potattributes/Potattributes.java
@@ -3,19 +3,32 @@ package com.teampotato.potattributes;
 import com.teampotato.potattributes.config.Config;
 import com.teampotato.potattributes.event.AttributeEvents;
 import com.teampotato.potattributes.registry.ModAttributes;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.EntityAttributeModificationEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.RegistryObject;
 
 @Mod(Potattributes.MOD_ID)
 public class Potattributes {
     public static final String MOD_ID = "potattributes";
 
     public Potattributes() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         MinecraftForge.EVENT_BUS.register(AttributeEvents.class);
-        ModAttributes.ATTRIBUTES.register(FMLJavaModLoadingContext.get().getModEventBus());
+        ModAttributes.ATTRIBUTES.register(bus);
+        bus.addListener(this::registerAttributes);
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.config);
+    }
+
+    private void registerAttributes(EntityAttributeModificationEvent event) {
+        for (RegistryObject<Attribute> attributeRegistryObject : ModAttributes.ATTRIBUTES.getEntries()) {
+            event.add(EntityType.PLAYER, attributeRegistryObject.get());
+        }
     }
 }
--- src/main/java/com/teampotato/potattributes/event/AttributeEvents.java
+++ src/main/java/com/teampotato/potattributes/event/AttributeEvents.java
@@ -1,30 +1,20 @@
 package com.teampotato.potattributes.event;
 
 import com.teampotato.potattributes.registry.ModAttributes;
-import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeInstance;
-import net.minecraftforge.event.entity.EntityAttributeModificationEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.registries.RegistryObject;
+import org.jetbrains.annotations.NotNull;
 
 public class AttributeEvents {
     @SubscribeEvent(priority = EventPriority.LOWEST)
-    public static void onLivingHurt(LivingHurtEvent event) {
+    public static void onLivingHurt(@NotNull LivingHurtEvent event) {
         LivingEntity livingEntity = event.getEntity();
         AttributeInstance attributeInstance = livingEntity.getAttribute(ModAttributes.DAMAGE_REDUCTION.get());
         if (attributeInstance == null || livingEntity.level.isClientSide || event.isCanceled()) return;
         double damageReduction = attributeInstance.getValue();
         event.setAmount(event.getAmount() * (1.0F - (float) damageReduction));
-    }
-
-    @SubscribeEvent
-    public static void registerAttributes(EntityAttributeModificationEvent event) {
-        for (RegistryObject<Attribute> attributeRegistryObject : ModAttributes.ATTRIBUTES.getEntries()) {
-            event.add(EntityType.PLAYER, attributeRegistryObject.get());
-        }
     }
 }
